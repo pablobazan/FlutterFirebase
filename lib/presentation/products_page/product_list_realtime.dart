bb799923f18cc0f5311bfcac10c757b14bbebad2
pablobazan <pablobazan6@gmail.com>
2023-07-16 19:53:14 -0300

    Fix getProducts in realtime page

--- lib/presentation/products_page/product_list_realtime.dart
+++ lib/presentation/products_page/product_list_realtime.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase/domain/entities/product.dart';
 import 'package:flutter_firebase/firebase/firebase_entities.dart';
@@ -17,34 +18,65 @@ class ProductListRealtime extends StatelessWidget {
         child: StreamBuilder(
           stream: getProducts(),
           builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-            } else if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (snapshot.connectionState == ConnectionState.active) {
-              final products = snapshot.data!.docs;
-              return ListView.builder(
-                itemCount: products.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(products[index].data().name),
-                    subtitle: Text(products[index].data().description),
-                    trailing: Text('\$${products[index].data().price}'),
-                    onLongPress: () {
-                      deleteProduct(context, products[index].id);
-                    },
-                  );
-                },
-              );
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                return const Center(
+                  child: Text('Not connected to the internet'),
+                );
+              case ConnectionState.waiting:
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              case ConnectionState.active:
+                final products = snapshot.data!.docs;
+                return ListView.builder(
+                  itemCount: products.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(products[index].data().name),
+                      subtitle: Text(products[index].data().description),
+                      trailing: Text('\$${products[index].data().price}'),
+                      onLongPress: () {
+                        deleteProduct(context, products[index].id);
+                      },
+                    );
+                  },
+                );
+              case ConnectionState.done:
+                return const SizedBox.shrink();
+              default:
+                return const SizedBox.shrink();
             }
 
-            if (snapshot.hasError) {
-              return const Center(
-                child: Text('Error'),
-              );
-            }
-            return const SizedBox.shrink();
+            // }
+            // if (snapshot.connectionState == ConnectionState.done) {
+            // } else if (snapshot.connectionState == ConnectionState.waiting) {
+            //   return const Center(
+            //     child: CircularProgressIndicator(),
+            //   );
+            // } else if (snapshot.connectionState == ConnectionState.active) {
+            //   final products = snapshot.data!.docs;
+            //   return ListView.builder(
+            //     itemCount: products.length,
+            //     itemBuilder: (context, index) {
+            //       return ListTile(
+            //         title: Text(products[index].data().name),
+            //         subtitle: Text(products[index].data().description),
+            //         trailing: Text('\$${products[index].data().price}'),
+            //         onLongPress: () {
+            //           deleteProduct(context, products[index].id);
+            //         },
+            //       );
+            //     },
+            //   );
+            // }
+
+            // if (snapshot.hasError) {
+            //   return const Center(
+            //     child: Text('Error'),
+            //   );
+            // }
+            // return const SizedBox.shrink();
           },
         ),
       ),
@@ -60,7 +92,7 @@ class ProductListRealtime extends StatelessWidget {
         );
 
     yield* productCollectionRef
-        // .where('userId', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+        .where('userId', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
         .snapshots();
   }
 
